84cecf0e6b69dc67fe3db749722e46b59b3d3cb5
Brian Lee <engbrianlee@gmail.com>
2020-07-27 21:06:35 -0400

    Prefetching. Closes #14

--- src/pages/PageShoppingLists.tsx
+++ src/pages/PageShoppingLists.tsx
@@ -4,15 +4,23 @@ import { Redirect, Switch, Route, useRouteMatch } from "react-router";
 import JoinedShoppingLists from "../components/shoppingList/JoinedShoppingLists";
 import Layout from "../layout/Layout";
 import { Link } from "react-router-dom";
+import { useGetJoinedShoppingListsLazyQuery } from "../generated/graphql";
 
 const PageShoppingLists = () => {
   let { path, url } = useRouteMatch();
+  // Created Shopping Lists is default route, we don't need to prefetch
+  // Pre fetch joined shopping lists
+  const [getJoinedShoppingLists] = useGetJoinedShoppingListsLazyQuery();
   return (
     <Layout>
       <Link className="text-blue-500" to={`${url}/created`}>
         Created
       </Link>
-      <Link className="text-blue-500" to={`${url}/joined`}>
+      <Link
+        className="text-blue-500"
+        to={`${url}/joined`}
+        onMouseOver={() => getJoinedShoppingLists()}
+      >
         Joined
       </Link>
       <Switch>
